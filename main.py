eb7eb8e7d155feaf46a765b042ca3d01d6b3db56
Michal.Tkaczyk <michaltkaczyk@outlook.com>
2021-09-07 09:19:04 +0200

    word and game classes

--- main.py
+++ main.py
@@ -7,6 +7,26 @@ class Word:
         return self.word
 
 
+class Game:
+
+    def __init__(self, word):
+        self.word = word
+        self.used_letters = set()
+
+    def ask_for_letter(self):
+        played_letter = input("Play a new letter: ")
+        self.used_letters.add(played_letter)
+
+    def show_letters(self):
+        print("You have already played:", ", ".join(sorted(self.used_letters)))
+
+
 if __name__ == '__main__':
-    word = Word("password")
-    print(word)
+    game = Game("password")
+
+    for x in range(3):
+        game.ask_for_letter()
+
+    game.show_letters()
+    print(game.word)
+
