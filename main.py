b09553ac2e74851605d3ec283531635f22f69b2f
Michal.Tkaczyk <michaltkaczyk@outlook.com>
2021-09-07 08:55:55 +0200

    hasing word

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ class Word:
     def __str__(self):
         return self.word
 
+    @property
+    def length(self):
+        return len(self.word)
+
+    def hide(self):
+        hashed_word = ""
+
+        for letter in self.word:
+            hashed_word += "_"
+
+        return hashed_word
+
+    def show(self):
+        return self.hide()
+
 
 if __name__ == '__main__':
     word = Word("password")
     print(word)
+    word.show()
